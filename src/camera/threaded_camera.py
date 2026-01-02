4b75fcb5939e47d574cfa7bdf82b9f2f78825c84
Tanvi <tanvi.chitturu@gmail.com>
2026-01-02 16:57:36 +0530

    Map filters to swipe gestures

--- src/camera/threaded_camera.py
+++ src/camera/threaded_camera.py
@@ -19,6 +19,8 @@ prev_x = None
 swipe_threshold = 0.08
 cooldown_time = 0.7
 last_swipe_time =0
+filters = ["Normal","B&W","Sepia","Cyberpunk","Warm"]
+current_filter = 0
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -43,9 +45,10 @@ while True:
                 dx = smoothed_x - prev_x
                 if abs(dx) > swipe_threshold and (current_time - last_swipe_time) > cooldown_time:
                     if dx > 0:
-                        print("Swipe Right Detected")
+                        current_filter = (current_filter+1)%len(filters)
                     else:
-                        print("Swipe Left Detected")
+                        current_filter = (current_filter-1)%len(filters)
+                        print("Filter:",filters[current_filter])
                     last_swipe_time = current_time
             prev_x = smoothed_x
             mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
@@ -53,7 +56,10 @@ while True:
     fps = 1/(curr_time-prev_time) if prev_time != 0 else 0
     prev_time = curr_time
     cv2.putText(frame, f"FPS: {int(fps)}",(10,40),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+    cv2.putText(frame, f"Filter: {filters[current_filter]}",(10,80),cv2.FONT_HERSHEY_PLAIN,2,(0,255,0),2)
     cv2.imshow("webcam",frame)
+
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
